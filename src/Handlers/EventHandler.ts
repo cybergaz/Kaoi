5b6226acac85e46b4909c04a43a416f596900017
cyber_gaz <43444535+cybergaz@users.noreply.github.com>
2022-01-26 11:55:29 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -19,11 +19,11 @@ export default class EventHandler {
         const text = add
             ? `- ${group.subject || '___'} -\n\n💠 *Group Description:*\n${
                   group.desc
-              }\n\nHope you follow the rules and have fun!\n*‣ ${event.participants
+              }\n\nwelcome my friend welcome  bada hi ajeeb janwar hota hai magarmachh...!\n*‣ ${event.participants
                   .map((jid) => `@${jid.split('@')[0]}`)
                   .join(', ')}*`
             : event.action === 'remove'
-            ? `*@${event.participants[0].split('@')[0]}* has left the chat 👋`
+            ? `*@${event.participants[0].split('@')[0]}* has left the chat , chalo bhago yaha se ...`
             : `*@${event.participants[0].split('@')[0]}* got ${this.client.util.capitalize(event.action)}d${
                   event.actor ? ` by *@${event.actor.split('@')[0]}*` : ''
               }`
